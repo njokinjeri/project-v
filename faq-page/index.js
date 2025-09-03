8e405efbf46bbd56025a61f448e6a4d4cb359fcd
NjokiNjeri <njerinjoki22@gmail.com>
2025-09-03 16:09:48 +0300

    faq-page complete

--- faq-page/index.js
+++ faq-page/index.js
@@ -6,3 +6,22 @@ buttons.forEach(btn => {
         btn.classList.add('active');
     });
 });
+
+const faqButtons = document.querySelectorAll('.faq-items button');
+
+faqButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        const expanded = button.getAttribute('aria-expanded') === 'true';
+        const content = document.getElementById(button.getAttribute('aria-controls'));
+
+        faqButtons.forEach((btn) => {
+            btn.setAttribute('aria-expanded', 'false');
+            document.getElementById(btn.getAttribute('aria-controls')).hidden = true;
+        });
+
+        if(!expanded) {
+            button.setAttribute('aria-expanded', 'true');
+            content.hidden = false;
+        }
+    });
+});
